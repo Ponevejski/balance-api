5b89af65589e0dbee1afe1c5b26c93cc8a5d6a77
Anton Paniavezhski <apaniavezhskia@flow.life>
2024-04-11 12:46:50 +0300

    change origin

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     }),
   );
